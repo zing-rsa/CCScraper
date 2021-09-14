4cfd52975f5dd7d58abb180274be8069b6ec291a
Robert Kritzinger <kritz.rob@gmail.com>
2021-09-14 09:23:00 +0200

    update deploy params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component'
 
 
 const routes: Routes = [
-  { path: '', component: AppComponent }
+  { path: 'CCScraper', component: AppComponent }
 ];
 
 @NgModule({
